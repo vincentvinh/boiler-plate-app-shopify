f90b2c0650479f35c210c0b2faf1ab96629d5bec
Your Name <vincent.christophe@nfq.asia>
2021-07-01 10:29:09 +0700

    [develop-api] presentation on dashboard

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -9,9 +9,18 @@ const Dashboard: FC = () => {
         <div className="text">
           <p>
             <img src="https://github.com/vincentvinh/boiler-plate-app-shopify" alt="" />
-            This is an example of how to build a complex web application using shopify Apis. Feel free to explore each
-            page and download the source code available on{' '}
-            <a target="_blank" rel="noreferrer" href="https://github.com/vincentvinh/boiler-plate-app-shopify">
+            <label>This is an example of how to build a complex web application using</label>
+            <ul>
+              <li>shopify Apis.</li>
+              <li>Google Auth</li>
+              <li>AWS S3 bucket</li>
+              <li>Firebase Admin for db</li>
+            </ul>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/vincentvinh/boiler-plate-app-shopify"
+            >
               Github
             </a>
             . Both the client and server are written in{' '}
@@ -28,7 +37,11 @@ const Dashboard: FC = () => {
             </a>
             . If you need tips on how to develop apps or just want to chat, use the{' '}
             <NavLink to="/messenger">Messenger</NavLink> page,{' '}
-            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/vincent-christophe">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/vincent-christophe"
+            >
               LinkedIn
             </a>{' '}
             or{' '}
@@ -38,6 +51,29 @@ const Dashboard: FC = () => {
             .
           </p>
         </div>
+      </div>
+      <div>
+        <NavLink className="icon" to="/shopify">
+          <i className="fas fa-shopping-cart" /> Shopify
+        </NavLink>
+        <ul>
+          <li>
+            Implements the{' '}
+            <a target="_blank" rel="noreferrer" href="https://shopify.dev/api/admin">
+              Shopify Admin Api
+            </a>{' '}
+            and{' '}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://shopify.dev/api/admin/graphql/reference"
+            >
+              Using Shopify graphql API
+            </a>
+          </li>
+          <li>Users load the product from shopify admin</li>
+          <li>Displays a list of products with filters and sort options</li>
+        </ul>
       </div>
       <div>
         <NavLink className="icon" to="/cooking">
